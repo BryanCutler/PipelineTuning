f16a041e58f39691e68856d84eca25962475be61
Bryan Cutler <cutlerb@gmail.com>
2018-05-09 15:17:13 -0700

    restrict to only allow one parent node

--- pipeline_tuning.py
+++ pipeline_tuning.py
@@ -33,36 +33,12 @@ from pyspark.sql.functions import rand
 __all__ = ['DagCrossValidator', 'DagPipeline']
 
 
-# Map the input param name to function to retrieve it
-def get_input_map():
-    return {
-        "inputCol": lambda params: None if not params.isSet("inputCol") else params.getInputCol(),
-        "inputCols": lambda params: None if not params.isSet("inputCols") else params.getInputCols(),
-        "featuresCol": lambda params: params.getFeaturesCol(),
-        "labelCol": lambda params: params.getLabelCol(),
-        "weightCol": lambda params: None if not params.isSet("weightCol") else params.getWeightCol(),
-    }
-
-
-# Map the output param name to function to retrieve it
-def get_output_map():
-    return {
-        "outputCol": lambda params: None if params.hasParam("outputCols") and params.isSet("outputCols") else params.getOutputCol(),
-        "outputCols": lambda params: None if not params.isSet("outputCols") else params.getOutputCols(),
-        "predictionCol": lambda params: params.getPredictionCol(),
-        "probabilityCol": lambda params: params.getProbabilityCol(),
-        "rawPredictionCol": lambda params: params.getRawPredictionCol(),
-        "varianceCol": lambda params: None if not params.isSet("varianceCol") else params.getVarianceCol(),
-    }
-
-
 class PipelineTreeNode(object):
 
-    def __init__(self, stage, stage_inputs, param_map, tree_type):
-        self.parents = []
+    def __init__(self, stage, param_map, tree_type):
+        self.parent = None
         self.children = []
         self.stage = stage
-        self.stage_inputs = stage_inputs
         self.param_map = param_map
         self.transformer = None
         self.lock = None
@@ -71,8 +47,8 @@ class PipelineTreeNode(object):
 
     def __repr__(self):
         param_map_str = str({p.name: v for p, v in self.param_map.iteritems()})
-        parent_map_str = str([{p.name: v for p, v in parent.param_map.iteritems()}
-                              for parent in self.parents])
+        parent_map_str = str([{} if self.parent is None else
+                              {p.name: v for p, v in self.parent.param_map.iteritems()}])
         return self.stage.uid + param_map_str + parent_map_str
 
     def cache_task(self, task, dataset):
@@ -103,8 +79,8 @@ class BFSTree(object):
 
     class TransformerNode(PipelineTreeNode):
 
-        def __init__(self, stage, stage_inputs, param_map):
-            super(BFSTree.TransformerNode, self).__init__(stage, stage_inputs, param_map, BFSTree)
+        def __init__(self, stage, param_map):
+            super(BFSTree.TransformerNode, self).__init__(stage, param_map, BFSTree)
 
         def transform_task(self, queue, results, dataset):
             transformed_dataset = self.transformer.transform(dataset)
@@ -174,8 +150,8 @@ class DFSTree(object):
 
     class TransformerNode(PipelineTreeNode):
 
-        def __init__(self, stage, stage_inputs, param_map):
-            super(DFSTree.TransformerNode, self).__init__(stage, stage_inputs, param_map, DFSTree)
+        def __init__(self, stage, param_map):
+            super(DFSTree.TransformerNode, self).__init__(stage, param_map, DFSTree)
 
         def transform_task(self, queue, results, holding, dataset):
             transformed_dataset = self.transformer.transform(dataset)
@@ -273,29 +249,22 @@ class IterableQueue(Queue.Queue):
 
 class DagPipeline(Pipeline, HasParallelism):
 
-    def __init__(self, stages, parallelism, tree_type="bfs", addl_inputs=None, addl_outputs=None):
+    def __init__(self, stages, parallelism, tree_type="bfs"):
         super(DagPipeline, self).__init__(stages=stages)
         self.setParallelism(parallelism)
         self.roots = None
         self.nodes = None
         self.tree_type = tree_type
-        self.input_map = get_input_map()
-        self.output_map = get_output_map()
-        if addl_inputs is not None:
-            self.input_map.update(addl_inputs)
-        if addl_outputs is not None:
-            self.output_map.update(addl_outputs)
 
     def evaluate(self, paramMaps, train, validation, eval):
         num_models = len(paramMaps)
         print('Num Models: %d, Parallelism: %d' % (num_models, self.getParallelism()))
 
-        if self.roots is None:
-            self.roots, self.nodes = self.build_dag(paramMaps)
+        # Build a DAG from the stages
+        self.roots, self.nodes = self.build_dag(paramMaps)
 
+        # Run tasks in queue using thread pool
         pool = ThreadPool(processes=min(self.getParallelism(), num_models))
-
-        # Task queue
         queue = IterableQueue()
 
         # Queue to store results
@@ -341,7 +310,7 @@ class DagPipeline(Pipeline, HasParallelism):
             model_params = {}
             while node is not None:
                 model_params.update(node.param_map)
-                node = node.parents[0] if node.parents else None  # TODO: multiple parents
+                node = node.parent
             metrics_params.append((metric, model_params))
         orig_order = {str(e): i for e, i in  zip(paramMaps, range(num_models))}
         metrics_params = sorted(metrics_params, key=lambda mp: orig_order[str(mp[1])])
@@ -355,6 +324,7 @@ class DagPipeline(Pipeline, HasParallelism):
         tree = BFSTree if self.tree_type.lower() == "bfs" else DFSTree
 
         stages = self.getStages()
+        stage_prev = None
 
         # Locate the last estimator, will not transform training data after fit
         for i, stage in enumerate(stages):
@@ -364,7 +334,6 @@ class DagPipeline(Pipeline, HasParallelism):
         nodes = []
         roots = []
         stage_nodes = {}
-        output_to_stage = {}
         for i, stage in enumerate(stages):
 
             # Determine what type of Node for the stage
@@ -383,58 +352,36 @@ class DagPipeline(Pipeline, HasParallelism):
                     if k.parent == stage.uid:
                         temp_map.setdefault(k, set()).add(v)
 
-            def flatten_list(l):
-                result = []
-                for i in l:
-                    if isinstance(i, list):
-                        for j in i:
-                            result.append(j)
-                    else:
-                        result.append(i)
-                return result
-
-            # Get the inputs/outputs for the stage
-            stage_inputs = flatten_list([f(stage) for name, f in self.input_map.iteritems()
-                                         if f is not None and stage.hasParam(name)])
-            stage_outputs = flatten_list([f(stage) for name, f in self.output_map.iteritems()
-                                          if f is not None and stage.hasParam(name)])
-            for output_col in stage_outputs:
-                output_to_stage[output_col] = stage
-
             # Check if have a param grid for this stage
             if temp_map:
                 grid_builder = ParamGridBuilder()
                 for k, v in temp_map.iteritems():
                     grid_builder.addGrid(k, v)
                 stage_param_grid = grid_builder.build()
-                new_nodes = [Node(stage, stage_inputs, param_map) for param_map in stage_param_grid]
+                new_nodes = [Node(stage, param_map) for param_map in stage_param_grid]
             else:
-                new_nodes = [Node(stage, stage_inputs, {})]
+                new_nodes = [Node(stage, {})]
 
-            # Find parent stage, assume stages in topological order
-            parent_stages = set(output_to_stage[i] for i in stage_inputs if i in output_to_stage)
-
-            # Make nodes for each node of parent stages
-            if parent_stages:
-                parent_nodes_list = [stage_nodes[parent_stage] for parent_stage in parent_stages]
-                parent_nodes_prod = product(*parent_nodes_list)
+            # Make nodes for each node of parent stage
+            if stage_prev:
                 temp_nodes = []
-                for parent_nodes in parent_nodes_prod:
+                parent_nodes = stage_nodes[stage_prev]
+                for parent_node in parent_nodes:
                     for node in new_nodes:
                         child_node = copy.copy(node)
                         child_node.children = []
-                        child_node.parents = parent_nodes
-                        for parent_node in parent_nodes:
-                            parent_node.children.append(child_node)
+                        child_node.parent = parent_node
+                        parent_node.children.append(child_node)
                         temp_nodes.append(child_node)
                 new_nodes = temp_nodes
             else:
                 roots += new_nodes
 
-            # Save the nodes that produce the output
+            # Store all new nodes created for this stage
             stage_nodes[stage] = new_nodes
-
             nodes += new_nodes
+
+            stage_prev = stage
 
         return roots, nodes
 
@@ -450,14 +397,12 @@ class DagPipeline(Pipeline, HasParallelism):
 
             g = nx.DiGraph()
             for node in nodes:
-                for parent in node.parents:
-                    g.add_edge(parent, node)
-
-            #topo = list(nx.topological_sort(g))
-
+                if node.parent is not None:
+                    g.add_edge(node.parent, node)
             if draw:
                 nx.draw(g, with_labels=True)
 
+            return g
         except ImportError as e:
             raise ImportError("Must install networkx to build graph\n%s" % e)
 
